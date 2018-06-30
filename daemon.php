05c23dc342ababeddc8c01b2f8a2c339b160cd1f
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-06-30 19:07:04 +0300

    add MotherDaemonClass into it

--- daemon.php
+++ daemon.php
@@ -8,14 +8,39 @@ $app = require_once __DIR__ . '/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+	$request = Illuminate\Http\Request::capture()
 );
 
+
 use App\Http\TicketBags\Secom;
-$secom = new Secom();
+use App\Models\Service;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
 
-//while (true) {
-    print_r($secom->getListTikets());
+class Daemon
+{
+	use \App\Http\TicketBags\MotherDaemon;
+}
 
-//    sleep(10);
-//}
+$pathOfTicketBags = config('services_arr.path');
+$service_model = new Service();
+# getting all Services which are equivalents 2 classes in app/Http/TicketBags/
+try {
+	$services = count($service_model->getServices())?$service_model->getServices():array('Secom');
+} catch (ModelNotFoundException $mnf) {
+	$services = array('Secom');
+	Log::error('Services not found!. Using default->Secom');
+}
+# concatenate path to class and service class
+$serviceClass = $pathOfTicketBags . $services[0];
+/*
+$Daemon = new Daemon($services[0]);
+$tickets = $Daemon->getTicketsFromService();
+$Daemon->storeData($tickets);
+*/
+
+$secom = new $serviceClass;
+print_r($secom->getListTikets());
+$ticket = $secom->getTiket(8530);
+print_r($ticket);
+
