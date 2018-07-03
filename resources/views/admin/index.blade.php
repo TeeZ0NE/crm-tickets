6c439bee3979cbb130edb91905efcf5e90939357
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-07-03 10:34:38 +0300

    Add laravel mix and run develop

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teez0ne
+ * Date: 03.07.18
+ * Time: 10:19
+ */
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,13 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+/*mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');*/
+mix.js([
+	'resources/assets/js/app.js',
+], 'public/js/app.js')
+	.stylus('resources/assets/stylus/app.styl', 'public/css');
+
+if (mix.inProduction()) {
+	mix.version();
+}
