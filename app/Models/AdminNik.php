54cab170663b33873ee5a910a534c8c1f3a4eaa9
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-07-13 12:41:01 +0300

    Placed model methods here from AdminController and add PHPDocs

--- app/Models/AdminNik.php
+++ app/Models/AdminNik.php
@@ -10,18 +10,39 @@ class AdminNik extends Model
     protected $fillable = array('service_id','admin_nik','admin_id');
 	protected $table = 'sysadmin_niks';
 
+	/**
+	 * getting service
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function getService()
 	{
 		return $this->belongsTo(Service::class, 'service_id', 'id');
 	}
 
+	/**
+	 * getting real admin with belongsTo
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function getAdmin()
 	{
 		return $this->belongsTo(Sysadmin::class,'admin_id','id');
 	}
 
+	/**
+	 * cross table get activity
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
 	public function getAdminActivity()
 	{
 		return $this->hasMany(SysadminActivity::class,'admin_nik_id', 'admin_nik_id');
+	}
+
+	/**
+	 * get sorted admin nicks 4 binding
+	 * @return AdminNik[]|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+	 */
+	public function getNicks()
+	{
+		return $this::with('getService')->orderBy('admin_nik')->get();
 	}
 }
