049b3050a49be8c9773537ed2c9fb0d482a1e098
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-07-25 12:25:01 +0300

    Add ru local. And Fonts

---
+++ resources/lang/ru/site.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teez0ne
+ * Date: 24.07.18
+ * Time: 12:44
+ */
+return[
+	'deadline'=>'Deadline',
+	'last replier'=>'Последний отвечавший',
+	'last reply'=>'Последний ответ',
+	'new'=>'Новый',
+	'priority'=>'Приоритет',
+	'service'=>'Клиент',
+	'status'=>'Статус',
+	'subject'=>'Тема',
+	'ticket id'=>'Тикет id',
+	'unknown' => 'Неизвестно',
+	'waiting time'=> 'Ожидание',
+	'assign'=>'Владелец',
+];
