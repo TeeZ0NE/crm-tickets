edcce6b219c10c6beebcc1f42ac81daee6beb77c
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-07-26 10:51:27 +0300

    Create script 4 getting statistics from files wich are placing (wrote) on services which use trait GetStatistic

---
+++ app/Http/TicketBags/GetStatistics.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teez0ne
+ * Date: 26.07.18
+ * Time: 9:00
+ */
+
+namespace App\Http\TicketBags;
+
+use Illuminate\Support\Facades\Log;
+
+trait GetStatistics
+{
+	public $result_arr = [];
+	private $compare_string = '';
+
+	/**
+	 * Check existing file
+	 *
+	 * @param $file_name
+	 * @return bool
+	 */
+	private function fileExist(string &$file_name)
+	{
+		$ch = curl_init($file_name);
+		curl_setopt($ch, CURLOPT_NOBODY, true);
+		curl_exec($ch);
+		$http_header = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		// $http >= 400 -> not found, $http = 200, found.
+		curl_close($ch);
+		switch ($http_header) {
+			case 200:
+				return True;
+			default:
+				return False;
+		}
+	}
+
+	/**
+	 * Get file context
+	 *
+	 * @param $file_name
+	 * @return array
+	 */
+	private function getFile(&$file_name)
+	{
+		$file = fopen($file_name, 'r');
+		$result_arr = [];
+		foreach ($this->getLine($file) as $val) {
+			if ($this->getCompareStr() !== $val)
+				$result_arr[] = $this->unsetTempArr($val);
+			$this->setCompareStr($val);
+		}
+		fclose($file);
+		return $result_arr;
+	}
+
+	/**
+	 * Get line from file
+	 * @param $file
+	 * @return \Generator
+	 */
+	private function getLine(&$file)
+	{
+		while (!feof($file)) {
+			$line = fgets($file);
+			yield $line;
+		}
+	}
+
+	/**
+	 * Get array from string
+	 * unset useless variables, replace double quotes
+	 *
+	 * @param $str
+	 * @return array
+	 */
+	private function unsetTempArr(string $str)
+	{
+		$temp_arr = explode(';', str_replace('"', '', $str));
+//		unset($temp_arr[1],$temp_arr[2]);
+		return $this->addKeys2Arr($temp_arr);
+	}
+
+	/**
+	 * add specific keys into array
+	 * @param array $temp_arr
+	 * @return array
+	 */
+	private function addKeys2Arr(array $temp_arr)
+	{
+		$res_arr = [];
+		foreach ($temp_arr as $key => $val) {
+			switch ($key) {
+				case 0:
+					$res_arr['lastreply'] = $val;
+					break;
+				case 3:
+					$res_arr['subject'] = trim($val);
+					break;
+				case 4:
+					$res_arr['admin'] = $val;
+					break;
+				case 5:
+					$res_arr['ticketid'] = $val;
+					break;
+			}
+		}
+		return $res_arr;
+	}
+
+	/**
+	 * Storing comparing string
+	 * @param $str
+	 */
+	private function setCompareStr(string $str): void
+	{
+		$this->compare_string = $str;
+	}
+
+	/**
+	 * Get stored comparing string
+	 * @return string
+	 */
+	private function getCompareStr()
+	{
+		return $this->compare_string;
+	}
+
+	/**
+	 * @param string $service
+	 * @param string $file_name
+	 * @return array
+	 */
+	public function getStatistic(string &$service,string &$file_name)
+	{
+			if ($this->fileExist($file_name)) {
+				$result_arr = $this->getFile($file_name);
+			}
+			else{
+				Log::error(sprintf('File %s$2 on service %s1$ not found',$service,$file_name));
+				$result_arr=[];
+			}
+		return $result_arr;
+	}
+}
---
+++ getStatistic.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teez0ne
+ * Date: 25.07.18
+ * Time: 18:54
+ */
+define('LARAVEL_START', microtime(true));
+require __DIR__ . '/vendor/autoload.php';
+
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+	$request = Illuminate\Http\Request::capture()
+);
+use Carbon\Carbon;
+class getStatistics
+{
+	use \App\Http\TicketBags\GetStatistics;
+}
+
+$files = ["adminvps" => 'http://91.235.128.62/adminvps/stat/adminvps_%s.stat.csv'];
+$yesterday = Carbon::now()->yesterday()->format('d.m.Y');
+foreach ($files as $service => $file) {
+	$file_name = sprintf($file, $yesterday);
+	$getStatistics = new getStatistics();
+	print_r($getStatistics->getStatistic($service,$file_name));
+}
+
+
+
