787ac9686e7de1de69b1c845c029f9de3d7b8c56
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-07-27 11:38:51 +0300

    Add methods for getting Service id and complication

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -17,4 +17,14 @@ class Service extends Model
 	{
 		return $this->pluck('id')->toArray();
     }
+
+	public function getServiceId(string $service):int
+	{
+		return $this->where('name',$service)->first()->id;
+    }
+
+	public function getCompl(int $service_id):float
+	{
+		return $this->find($service_id)->first()->compl;
+    }
 }
