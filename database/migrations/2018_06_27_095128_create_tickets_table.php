c48359d41ec8ad0fd96dfc3ae4f0e23046e19f6f
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-08-02 13:59:29 +0300

    remove compl column

--- database/migrations/2018_06_27_095128_create_tickets_table.php
+++ database/migrations/2018_06_27_095128_create_tickets_table.php
@@ -20,7 +20,6 @@ class CreateTicketsTable extends Migration
 	        $table->unsignedTinyInteger('service_id')->comment('secom etc');
 	        $table->unsignedTinyInteger('status_id')->nullable()->comment('status_id');
 	        $table->unsignedTinyInteger('priority_id')->nullable()->comment('priority');
-	        $table->unsignedDecimal('compl',2,1)->default(1)->comment('difficulty');
 	        $table->dateTime('lastreply')->comment('Is comming from out');
 	        $table->unsignedSmallInteger('last_replier_nik_id')->default(0)->comment('nik ID 4 last replier');
 	        $table->unsignedInteger('user_assign_id')->nullable()->comment('user id which assign ticket');
