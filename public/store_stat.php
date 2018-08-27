d2585249bea2e67ec8a2c0fb81336ec9a4b5e687
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-08-27 12:01:50 +0300

    Add folder 4 sender where store_stat.php will store a file statistic, same format as adminvps

---
+++ public/store_stat.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teez0ne
+ * Date: 22.08.18
+ * Time: 12:25
+ */
+
+//print_r($_SERVER['REQUEST_METHOD']);
+header('Access-Control-Allow-Origin: *');
+header('Pragma: no-cache');
+header('Content-type: text/plain');
+$service = $_REQUEST['service']??'test';
+//if (isset($_REQUEST['service])){unset($_REQUEST['service']);}
+$obj = $_REQUEST;
+$curr_date =  date('d.m.Y',time());
+file_put_contents(
+	sprintf('./../storage/stats/%2$s_%1$s.stat.csv',$curr_date,$service),
+	implode(';',$obj).PHP_EOL,FILE_APPEND|LOCK_EX
+);
+//echo ob_get_length();
+//print_r(get_headers($_SERVER['HTTP_REFERER'],1));
