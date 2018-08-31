43f239084b265ff85697dcd1f86c24bb42efdc00
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-08-31 18:13:30 +0300

    Replace Jq AJAX method 2 xhr

--- public/store_stat.php
+++ public/store_stat.php
@@ -14,9 +14,11 @@ $service = $_REQUEST['service']??'test';
 //if (isset($_REQUEST['service])){unset($_REQUEST['service']);}
 $obj = $_REQUEST;
 $curr_date =  date('d.m.Y',time());
+
 file_put_contents(
 	sprintf('./../storage/stats/%2$s_%1$s.stat.csv',$curr_date,$service),
 	implode(';',$obj).PHP_EOL,FILE_APPEND|LOCK_EX
 );
+echo "ok skript";
 //echo ob_get_length();
 //print_r(get_headers($_SERVER['HTTP_REFERER'],1));
