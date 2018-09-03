6ffac66bbbfa3391c1becebfcb03e472786b75be
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-03 15:22:37 +0300

    Add trait store statistic

--- public/store_stat.php
+++ public/store_stat.php
@@ -10,17 +10,22 @@
 //header('Access-Control-Allow-Origin: *');
 header('Pragma: no-cache');
 header('Content-type: text/plain');
-$service = $_REQUEST['service']??'test';
-//if (isset($_REQUEST['service])){unset($_REQUEST['service']);}
-$obj = $_REQUEST;
-$curr_date =  date('d.m.Y',time());
+class storeStatistics
+{
+	use \App\Http\TicketBags\StoreStatistic;
+}
+echo 'Data received'.PHP_EOL;
+$service = $_REQUEST['service']??'Unknown';
+if (isset($_REQUEST['service'])){unset($_REQUEST['service']);}
+$get_stat_arr = $_REQUEST;
+//$curr_date =  date('d.m.Y',time());
 
-file_put_contents(
+/*file_put_contents(
 	sprintf('./../storage/stats/%2$s_%1$s.stat.csv',$curr_date,$service),
 	implode(';',$obj).PHP_EOL,FILE_APPEND|LOCK_EX
-);
-echo "ok skript";
-printf('service %s',$service);
-print_r($obj);
+);*/
+$storeStatistic = new storeStatistics($service,$get_stat_arr);
+$storeStatistic->store();
+
 //echo ob_get_length();
 //print_r(get_headers($_SERVER['HTTP_REFERER'],1));
