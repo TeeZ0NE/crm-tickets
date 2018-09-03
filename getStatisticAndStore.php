b10c136d073d702a8e41cb86dd15fed445b4b0a1
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-03 16:28:45 +0300

    Add require_once

--- getStatisticAndStore.php
+++ getStatisticAndStore.php
@@ -6,10 +6,9 @@
  * Time: 18:54
  */
 define('LARAVEL_START', microtime(true));
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $app = require_once __DIR__ . '/bootstrap/app.php';
-
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
