69939dc1609199f155f76f84cef238e531f60765
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-03 15:03:31 +0300

    Add debug temporary

--- public/store_stat.php
+++ public/store_stat.php
@@ -20,5 +20,7 @@ file_put_contents(
 	implode(';',$obj).PHP_EOL,FILE_APPEND|LOCK_EX
 );
 echo "ok skript";
+printf('service %s',$service);
+print_r($obj);
 //echo ob_get_length();
 //print_r(get_headers($_SERVER['HTTP_REFERER'],1));
