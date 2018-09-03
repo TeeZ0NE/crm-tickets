f813c494b7fa626b734a6e486f553a90dbcc73e0
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-03 15:36:12 +0300

    debug

--- public/store_stat.php
+++ public/store_stat.php
@@ -10,11 +10,12 @@
 //header('Access-Control-Allow-Origin: *');
 header('Pragma: no-cache');
 header('Content-type: text/plain');
-class storeStatistics
+/*class storeStatistics
 {
 	use \App\Http\TicketBags\StoreStatistic;
-}
+}*/
 echo 'Data received'.PHP_EOL;
+/*
 $service = $_REQUEST['service']??'Unknown';
 if (isset($_REQUEST['service'])){unset($_REQUEST['service']);}
 $get_stat_arr = $_REQUEST;
@@ -24,8 +25,9 @@ $get_stat_arr = $_REQUEST;
 	sprintf('./../storage/stats/%2$s_%1$s.stat.csv',$curr_date,$service),
 	implode(';',$obj).PHP_EOL,FILE_APPEND|LOCK_EX
 );*/
+/*
 $storeStatistic = new storeStatistics($service,$get_stat_arr);
 $storeStatistic->store();
-
+*/
 //echo ob_get_length();
 //print_r(get_headers($_SERVER['HTTP_REFERER'],1));
