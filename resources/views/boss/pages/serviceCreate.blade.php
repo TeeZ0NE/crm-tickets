6199883310633767962f9f9cf319236d02620b62
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-04 12:34:36 +0300

    Creating service

---
+++ resources/views/boss/pages/serviceCreate.blade.php
@@ -0,0 +1,30 @@
+@extends('boss.layout')
+@section('title','Клиенты')
+@section('main_content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 mb-md-2 mb-lg-3">
+				<div class="card">
+					<h5 class="card-header">Клиенты (тикетницы)</h5>
+					<div class="card-body">
+						<form action="{{route('services.create')}}" method="post">
+							@csrf
+							<div class="input-group">
+								<div class="input-group-prepend">
+									<span class="input-group-text">Клиент, сложность</span>
+								</div>
+								<input type="text" aria-label="name" class="form-control"
+								       value="{{old('name')}}" name="name">
+								<input type="number" aria-label="compl" class="form-control"
+								       value="@php echo old('compl')??1.0; @endphp" step=".1" name="compl">
+								<button type="submit" class="store-service btn btn-info">
+									<i class="fas fa-save"></i>
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
