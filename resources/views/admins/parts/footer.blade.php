ce1c3f01e1dc1875c0a56d38eec01568a71e44af
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-04 12:40:45 +0300

    Add displaing in a footer

--- resources/views/admins/parts/footer.blade.php
+++ resources/views/admins/parts/footer.blade.php
@@ -1,3 +1,4 @@
 @stack('js-scripts')
 <p>admins footer</p>
+<p>{{config('app.version')}}</p>
 @include('parts.refresh_at')
--- resources/views/boss/parts/footer.blade.php
+++ resources/views/boss/parts/footer.blade.php
@@ -1,3 +1,4 @@
 @stack('js-scripts')
 <p>boss footers</p>
+<p>{{config('app.version')}}</p>
 @include('parts.refresh_at')
