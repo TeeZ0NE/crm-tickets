a0a3710b7567036ab26d5f52c7ad0ecaee09eb4f
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-09-11 14:16:51 +0300

    New version

--- config/app.php
+++ config/app.php
@@ -214,6 +214,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 	    'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
-"version"=>'v1.0.2',
+"version"=>'v1.0.3',
 	'faker_locale'=>'en',
 ];
