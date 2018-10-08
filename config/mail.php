a98d4b5332014cd234ece8d0a33bc34131302fdc
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-10-08 10:13:49 +0300

    Add settings and theme

--- config/mail.php
+++ config/mail.php
@@ -1,26 +1,43 @@
 <?php
 
-return array(
-	"driver" => "smtp",
-	"host" => "smtp.ukr.net",
-	"port" => 2525,
-	"encryption"=>"ssl",
-	"from" => array(
-		"address" => "endnet@ukr.net",
-		"name" => "Admin admin-area"
-	),
-	"username" => "endnet@ukr.net",
-	"password" => "ZW5kbmV0QHVrci5uZXQ=",
-	"sendmail" => "/usr/sbin/sendmail -bs",
-	"pretend" => false,
-	'markdown' => [
-		'theme' => 'default1',
-
-		'paths' => [
-			resource_path('views/vendor/mail'),
-		],
+return
+	array("driver" => "smtp",
+		"host" => "atlas.secom.com.ua",
+		"port" => 465,
+		"encryption" => "ssl",
+		"from" => array(
+			"address" => "admin-area@secom.com.ua",
+			"name" => "Admin admin-area"
+		),
+		"username" => "admin-area@secom.com.ua",
+		"password" => "moqC7Qb2f8616AfZ",
+		"sendmail" => "/usr/sbin/sendmail -bs",
+		"pretend" => false,
+		'markdown' => [
+			'theme' => 'default1',
+			'paths' => [
+				resource_path('views/vendor/mail'),
+			],]);
+/*array(
+"driver" => "smtp",
+"host" => "smtp.ukr.net",
+"port" => 2525,
+"encryption"=>"ssl",
+"from" => array(
+	"address" => "endnet@ukr.net",
+	"name" => "Admin admin-area"
+),
+"username" => "endnet@ukr.net",
+"password" => "ZW5kbmV0QHVrci5uZXQ=",
+"sendmail" => "/usr/sbin/sendmail -bs",
+"pretend" => false,
+'markdown' => [
+	'theme' => 'default1',
+	'paths' => [
+		resource_path('views/vendor/mail'),
 	],
-);
+],
+);*/
 /* mailtrap
 return array(
 	"driver" => "smtp",
