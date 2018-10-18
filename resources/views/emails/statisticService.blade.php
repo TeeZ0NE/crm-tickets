b84d095751f13214c0aaef3388d534e15439237a
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-10-18 18:21:21 +0300

    Add foter in mail blade

--- resources/views/emails/statisticService.blade.php
+++ resources/views/emails/statisticService.blade.php
@@ -2,4 +2,13 @@
 <div class="table-holder">
 	@include('boss.parts.service_statistic_by_interval')
 </div>
+<div class="signature">
+	<h3>ServerCommunications</h3>
+	<a href="http://secom.com.ua">Secom</a><br>
+	<a href="email:support@secom.com.ua">support@secom.com.ua</a><br>
+	<a href="tel:+380443839153">+38.044.383.91.53"</a><br>
+	<a href="tel:+380432527387">+38.0432.52.73.87</a><br>
+	<a href="tel:+74996772058">+7.499.677.20.58</a><br>
+	Skype: <a href="skype://secom.com.ua">secom.com.ua</a>
+</div>
 
