7ba233c58d9b051bf6570a68864e8c4013d0b33f
TeeZ0NE <teez0ne.flyI4@gmail.com>
2018-10-31 17:19:40 +0200

    Fix carbon in admin stats

--- resources/views/admins/pages/statisticsByMonth.blade.php
+++ resources/views/admins/pages/statisticsByMonth.blade.php
@@ -6,8 +6,8 @@
 			<div class="col-md-12 mb-md-2 mb-lg-3">
 				@foreach($statistic4AllAdminsByMonths as $statistic4AllAdminsByMonth)
 					@include('boss.parts.ratesByMonth',[
-					'month'=>$Carbon->now()->subMonth($iterator)->startOfMonth()->toDateString(),
-					'endMonth' => $Carbon->now()->subMonth($iterator)->endOfMonth()->toDateString(),
+					'month'=>$Carbon->now()->startOfMonth()->subMonth($iterator)->toDateString(),
+					'endMonth' => $Carbon->now()->endOfMonth()->subMonth($iterator)->toDateString(),
 					'statistic4AllAdmins'=>$statistic4AllAdminsByMonth
 					])
 					@php $iterator--;@endphp
